67ab8cfa33e1a650ff7b53d84388b9c9aefca272
Prashant Tak <prashantrameshtak@gmail.com>
2021-04-14 21:31:06 +0530

    Change parameters to mach spec of the source

--- src/meta_bilstm/bin/train_model.py
+++ src/meta_bilstm/bin/train_model.py
@@ -7,36 +7,41 @@ from meta_bilstm.train_utils.trainer import Trainer
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--embeddings_path", "-e",
+        "--embeddings_path",
+        "-e",
         type=str,
         default="data/embeddings/model.txt",
         help="path to pretrained embeddings",
     )
     parser.add_argument(
-        "--config-path", "-c",
+        "--config-path",
+        "-c",
         type=str,
         default="config/model_config.json",
         help="path to model config",
     )
     parser.add_argument(
-        "--train-data", "-t",
+        "--train-data",
+        "-t",
         type=str,
         default="data/ru_syntagrus-ud-train.conllu",
-        help="path to training data in conllu format"
+        help="path to training data in conllu format",
     )
+    # TODO Add argument for choosing between lstm or gru
     parser.add_argument(
-        "--device", "-d",
+        "--device",
+        "-d",
         type=str,
         default="cpu",
         help="device to use for model training/inference",
     )
     args = parser.parse_args()
 
-    with open(args.config_path, 'r') as f:
+    with open(args.config_path, "r") as f:
         model_config = json.load(f)
 
     trainer = Trainer(model_config, args.device, args.train_data)
-    trainer.train_model(5)
+    trainer.train_model(3)
 
 
 if __name__ == "__main__":
